9ec97fe320eaa45dbe84877cf1442560f6b193b7
Marcio Fernandes <farciomernandes@gmail.com>
2023-02-22 11:07:11 -0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  setDocumentation(app);
   await app.listen(3000);
 }
+
+const setDocumentation = (app) => {
+  const config = new DocumentBuilder()
+    .setTitle('Crud nest with swagger and test')
+    .setDescription('Documentation app')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/docs', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+    customSiteTitle: 'Crud nest with swagger and test',
+  });
+};
+
 bootstrap();
