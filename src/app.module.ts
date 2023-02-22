8c6be626b01b1a35ce475e5cb66760196da4cc12
Marcio Fernandes <farciomernandes@gmail.com>
2023-02-22 16:24:50 -0300

    feat: import modules user and typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { UserController } from './presentation/user.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmDataSource } from './infra/db/database.provider';
+import { UserModule } from './infra/modules/user/user.module';
 
 @Module({
   imports: [
@@ -9,8 +12,17 @@ import { ConfigModule } from '@nestjs/config';
       envFilePath: ['.env'],
       isGlobal: true,
     }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: () => ({}),
+      dataSourceFactory: async () => {
+        const dataSource = await TypeOrmDataSource.initialize();
+        return dataSource;
+      },
+    }),
+    UserModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
